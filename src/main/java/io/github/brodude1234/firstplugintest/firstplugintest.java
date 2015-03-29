5bc6da827241f38b9d86c0095ae925b19fe5fbbc
Brodude1234 <Brodude1234@users.noreply.github.com>
2015-03-30 06:59:06 +1000

    Setup Cause I know How it Works Now
    Ye

---
+++ src/main/java/io/github/brodude1234/firstplugintest/firstplugintest.java
@@ -0,0 +1,30 @@
+package io.github.brodude1234.firstplugintest;
+
+/**
+ * Created by Brodude1234 on 25/03/2015.
+ */
+
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
+public final class firstplugintest extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("The firstplugintest plugin onEnable has been invoked!");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("The firstplugintest plugin onDisable has been invoked!");
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (cmd.getName().equalsIgnoreCase("basic")) {
+            getLogger().info("The basic command has successfully run! Hooray!");
+            return true;
+        }
+        return false;
+    }
+}
