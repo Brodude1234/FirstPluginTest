c71ad06b481eac1342e4f1324bc871fac2a88678
Brodude1234 <Brodude1234@users.noreply.github.com>
2015-03-30 15:11:02 +1000

    Implemented 2nd Command Wrongly
    Work for later!

--- src/main/java/io/github/brodude1234/firstplugintest/firstplugintest.java
+++ src/main/java/io/github/brodude1234/firstplugintest/firstplugintest.java
@@ -4,6 +4,8 @@ package io.github.brodude1234.firstplugintest;
  * Created by Brodude1234 on 25/03/2015.
  */
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -20,6 +22,30 @@ public final class firstplugintest extends JavaPlugin {
             getLogger().info("The basic command has successfully run! Hooray!");
             sender.sendMessage("You run dat command! My name's Jeff!");
             return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (cmd.getName().equalsIgnoreCase("ignite")) {
+          if (args.length != 1) {
+              return false;
+          }
+          if (!(sender instanceof Player)){
+            sender.sendMessage("You can't hurt people if they cant hurt you! Meanie!");
+          }
+
+          Player target = Bukkit.getServer().getPlayer(args[0]);
+
+          if (target == null) {
+              sender.sendMessage(args[0] + "is not online. Sucked in!");
+              return true;
+          }
+
+            target.setFireTicks("100");
+            return true;
+
         }
         return false;
     }
